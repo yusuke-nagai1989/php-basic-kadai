5dc0d9f701087a27b2554d9b76c1a86bf324ae3c
yusuke-nagai1989 <156492699+yusuke-nagai1989@users.noreply.github.com>
2024-03-17 03:44:27 +0900

    PHP_kadai_007のアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="utf-8">
+    <title>PHP課題_5</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $vegetable = ['name' => 'onion','price' => 200,'weight' => 160];
+      print_r($vegetable);
+      ?>
+    </p>
+  </body>
+</html>
